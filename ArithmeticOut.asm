0dda2eabcfd419a036521de598d4fe20637738ae
Chris Villarreal <cjvillar@users.noreply.github.com>
2024-11-19 07:27:31 -0800

    update outfile

--- ArithmeticOut.asm
+++ ArithmeticOut.asm
@@ -3,23 +3,27 @@ result: .asciz "Result: %ld\n"
     .text
     .globl _start
 _start:
-    mov $1, %rax
+    mov $41, %rax
     push %rax
-    mov $5, %rax
+    mov $378, %rax
     pop %rbx
     add %rbx, %rax
     push %rax
-    mov $2, %rax
+    mov $19, %rax
+    push %rax
+    mov $54, %rax
+    push %rax
+    mov $6, %rax
+    pop %rbx
+    sub %rax, %rbx
+    mov %rbx, %rax
     pop %rbx
     mov %rax, %rcx
     mov %rbx, %rax
     cqto    #Convert quadword in %rax to octoword in %rdx:%rax
     idivq %rcx
-    push %rax
-    mov $2, %rax
     pop %rbx
-    sub %rax, %rbx
-    mov %rbx, %rax
+    imulq %rbx, %rax
 
 movq %rax, %rsi
 lea result(%rip), %rdi
